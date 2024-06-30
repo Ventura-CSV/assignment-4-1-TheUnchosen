52abbc88e35de95bf8776e0a98d17d09975e5e9c
TheUnchosen <longbui369@gmail.com>
2024-06-30 06:50:38 +0000

    2222222

--- main.py
+++ main.py
@@ -1,16 +1,15 @@
 def main():
     result = []
     start = input('Enter the starting letter: ')
-    while start.isalpha() == False:
-        start = input('Enter a valid starting letter: ')
     end = input('Enter the ending letter : ')
-    while end.isalpha() == False:
+    while start.isalpha() == False or (ord(end) < ord(start)):
+        start = input('Enter a valid starting letter: ')
         end = input('Enter a valid ending letter : ')
         
-    for count in range(ord(start),ord(end)):
+    for count in range(ord(start),ord(end) + 1):
         result.append(start)
         start = chr(ord(start) + 1)
-        
+
     """
     ########################################
     Code Your Program here
